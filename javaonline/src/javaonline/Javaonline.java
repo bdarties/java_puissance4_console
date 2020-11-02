3c3019b8da91d960ce57c989569924725c9b07bf
bdarties <benoit.darties@gmail.com>
2020-11-02 09:12:30 +0100

    nouveau projet

--- javaonline/src/javaonline/Javaonline.java
+++ javaonline/src/javaonline/Javaonline.java
@@ -17,6 +17,7 @@ public class Javaonline {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello");
+        // hello
     }
     
 }
