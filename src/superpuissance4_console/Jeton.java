bdfe815a5f35fea5e0f4369e4ed06140109d846d
bdarties <benoit.darties@gmail.com>
2020-11-03 14:13:59 +0100

    initialisation du dépot

---
+++ src/superpuissance4_console/Jeton.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package superpuissance4_console;
+
+/**
+ *
+ * @author bdarties
+ */
+public class Jeton {
+    String Couleur;
+    
+    Jeton(String coul){
+        Couleur = coul;
+    }
+    
+    String lireCouleur(){
+        return Couleur;
+    }
+    
+    @Override
+    public String toString(){
+        if("Rouge".equals(Couleur))
+            return "\u001B[31m O ";
+        return "\u001B[33m O ";
+    }
+}
